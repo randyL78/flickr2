a02c875c3a69959b38d89e8aee5a50b6204f5d7b
Randy's Mac <randydavidl78@gmail.com>
2018-12-21 17:47:02 -0500

    Fix package vulnerabilities, and the bugs the audit caused

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -38,16 +38,18 @@ const SearchInput = styled.input `
  * SearchForm component to prompt user to enter a search term
  * to use in Flickr API
  */
-const SearchStateless = props =>  
-  <Form className="search-F" onSubmit={ e => props.handleSubmit(e, this.searchField, props.history) }>
-    <SearchInput type="search" name="search" placeholder="Search" required innerRef={ input => this.searchField = input} />
-    <SearchButton type="submit">
-      <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-        <path d="M0 0h24v24H0z" fill="none"/>
-      </svg>
-    </SearchButton>
-  </Form>
-;
+const SearchStateless = props =>  {
+  let searchField;
+  return (
+    <Form className="search-F" onSubmit={ e => props.handleSubmit(e, searchField, props.history) }>
+      <SearchInput type="search" name="search" placeholder="Search" required innerRef={ input => searchField = input} />
+      <SearchButton type="submit">
+        <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+          <path d="M0 0h24v24H0z" fill="none"/>
+        </svg>
+      </SearchButton>
+    </Form>
+  )};
 
 export default SearchStateless;
