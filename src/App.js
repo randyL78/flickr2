a0075ac3075021f61b5b54eb1b80a7f2de17bc3b
Randy's Mac <randydavidl78@gmail.com>
2018-12-21 14:37:13 -0500

    Move local searchform state to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // React libraries
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 
 // Library for styling React components inside of component itself
@@ -10,6 +10,7 @@ import SearchForm from './Components/SearchForm/SearchForm';
 import Navigation from './Components/Navigation/Nav';
 import PhotoContainer from './Components/PhotoContainer/PhotoContainer';
 import ErrorPage from './Components/ErrorPage';
+
 
 /* Create styles for div.container elements */
 const Container = styled.div `
@@ -25,24 +26,43 @@ const Container = styled.div `
 `
 
 /**
- * Main App component responsible for handling routes and redirection
+ * Main App component responsible for handling routes, redirection, and managing major state
  */
-const App = () => 
-  <BrowserRouter>
-    <Container>
-      <Route exact path = "/" render={() => <Redirect to="/cats" />} />
-      <Route path = "/search" component={SearchForm} />
-      <Navigation links={['Cats', 'Dogs', 'Computers', 'Search']} />
-      <Switch>
-        <Route exact path = "/search" render={ () => <PhotoContainer searchTerm="cats"/> } />
-        <Route path = "/search/:searchTag" render={ props => <PhotoContainer searchTerm={props.match.params.searchTag} />} />
-        <Route path = "/cats" render={ () => <PhotoContainer searchTerm="cats"/>} />
-        <Route path = "/dogs" render={ () => <PhotoContainer searchTerm="dogs"/>} />
-        <Route path = "/computers" render={ () => <PhotoContainer searchTerm="computers"/> } />
-        <Route component={ErrorPage} />
-      </Switch>
-    </Container>
-  </BrowserRouter>
+class App extends Component {
 
+  state = {
+    // number of photos to pull and display at a time
+    numberOfPhotos: 24,
+    isLoading: true,
+    // array to contain API results
+    photos: [],
+  };
+
+  handleSubmit = (e, searchField, history) => {
+    e.preventDefault();
+    let path = `/search/${searchField.value}`;
+    searchField.value = "";
+    history.push(path);
+  }
+
+  render() {
+    return (
+    <BrowserRouter>
+      <Container>
+        <Route exact path = "/" render={() => <Redirect to="/cats" />} />
+        <Route path = "/search" render={ props => <SearchForm history={ props.history }  handleSubmit= { this.handleSubmit } /> }   />
+        <Navigation links={['Cats', 'Dogs', 'Computers', 'Search']} />
+        <Switch>
+          <Route exact path = "/search" render={ () => <PhotoContainer searchTerm="cats"/> } />
+          <Route path = "/search/:searchTag" render={ props => <PhotoContainer searchTerm={props.match.params.searchTag} />} />
+          <Route path = "/cats" render={ () => <PhotoContainer searchTerm="cats"/>} />
+          <Route path = "/dogs" render={ () => <PhotoContainer searchTerm="dogs"/>} />
+          <Route path = "/computers" render={ () => <PhotoContainer searchTerm="computers"/> } />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+    )}
+}
 
 export default App;
